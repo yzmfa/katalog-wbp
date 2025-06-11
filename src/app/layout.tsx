1649d9d01c932ec1dd75f7270151bd6603998822
Yusuf Zaenul Mustofa <yusufmustofa930@gmail.com>
2025-06-12 02:02:54 +0700

    yok fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
@@ -29,7 +30,9 @@ export default function RootLayout({
         />
       </head>
       <body className={`${inter.className} h-full flex flex-col`}>
-        <SmoothScroll />
+        <Suspense>
+          <SmoothScroll />
+        </Suspense>
         <Header />
         <main className="pt-12 flex-grow">
           {children}
